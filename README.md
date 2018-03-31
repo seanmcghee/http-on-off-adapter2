7caee1f60df58d10c279d3abe947baa30009fa43
Sean McGhee <37956209+seanmcghee@users.noreply.github.com>
2018-03-31 14:16:31 +0100

    initial upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# mozilla-iot-adapter
+Adapter for use in conjunction with Sonoff Basic device
+
+This adapter presents a multiple sonoff devices, which represents the power status of each device, i.e. 'ON' or 'OFF'..
---
+++ http-on-off-adapter2.js
@@ -0,0 +1,136 @@
+/**
+ * http-on-off-adapter2.js - OnOff adapter implemented as a plugin.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+'use strict';
+
+const fetch = require('node-fetch');
+var mqtt = require('mqtt');
+
+let Adapter, Device, Property;
+try {
+  Adapter = require('../adapter');
+  Device = require('../device');
+  Property = require('../property');
+} catch (e) {
+  if (e.code !== 'MODULE_NOT_FOUND') {
+    throw e;
+  }
+
+  const gwa = require('gateway-addon');
+  Adapter = gwa.Adapter;
+  Device = gwa.Device;
+  Property = gwa.Property;
+}
+
+class HttpOnOffProperty2 extends Property {
+  constructor(device, name, propertyDescription) {
+    super(device, name, propertyDescription);
+    this.unit = propertyDescription.unit;
+    this.description = propertyDescription.description;
+
+    // In an ideal world, we would query the device and return
+    // it's value.
+    this.setCachedValue(propertyDescription.value);
+    this.device.notifyPropertyChanged(this);
+  }
+
+  /**
+   * @method setValue
+   * @returns a promise which resolves to the updated value.
+   *
+   * @note it is possible that the updated value doesn't match
+   * the value passed in.
+   */
+  setValue(value) {
+    return new Promise((resolve, reject) => {
+      
+      let mqttTopic = 'cmd/' + this.device.mqttid + '/POWER';
+      let mqttPayload = '';
+      
+      if (value) {
+        //turn light on
+        mqttPayload = 'ON';
+      } else {
+        //turn light off
+        mqttPayload = 'OFF';
+      }
+      
+      var client = mqtt.connect({ port: 1883, host: '192.168.0.2', username: 'admin', password: 'sm681600', keepalive: 10000});
+      console.log('MQQT Client connected');
+      
+      try {
+      
+        console.log('About to publish to MQQT Client. Topic:',mqttTopic, 'Payload:', mqttPayload);
+        
+        client.on('connect', function () {
+            client.publish(mqttTopic, mqttPayload);
+            console.log('Published to MQQT Client. Topic:',mqttTopic, 'Payload:', mqttPayload);
+            client.end(); 
+            console.log('MQQT Client disconnected');
+        })
+        
+        
+        this.setCachedValue(value);
+        console.log('Property:', this.name, 'set to:', mqttPayload, 'using topic:', mqttTopic);
+        resolve(value);
+        this.device.notifyPropertyChanged(this);
+        
+      } catch (e) {
+        console.error('Request to mqtt topic:', mqttTopic, 'failed');
+        console.error(e);
+        reject(e);
+      }
+
+            
+    });
+  }
+}
+      
+
+class HttpOnOffDevice2 extends Device {
+  constructor(adapter, id, mqttid) {
+    super(adapter, id);
+
+    this.name = 'Light ' + mqttid;
+    this.type = 'onOffLight';
+    this.description = 'Simple HTTP OnOff Light';
+    this.mqttid = mqttid;
+
+    console.log('Device MQTT Id:', mqttid, 'added');
+
+    this.properties.set('on', new HttpOnOffProperty2(this, 'on', {
+      type: 'boolean',
+      value: false,
+    }));
+  }
+}
+
+class HttpOnOffAdapter2 extends Adapter {
+  constructor(addonManager, packageName) {
+    super(addonManager, 'HttpOnOffAdapter2', packageName);
+    addonManager.addAdapter(this);
+  }
+}
+
+function loadHttpOnOffAdapter2(addonManager, manifest, _errorCallback) {
+  let adapter = new HttpOnOffAdapter2(addonManager, manifest.name);
+
+  adapter.handleDeviceAdded(new HttpOnOffDevice2(adapter,
+                                                'HttpOnOffDevice2-01',
+                                                "sonoff"));
+                                                
+  adapter.handleDeviceAdded(new HttpOnOffDevice2(adapter,
+                                                'HttpOnOffDevice2-02',
+                                                "sonoff2"));
+  
+  adapter.handleDeviceAdded(new HttpOnOffDevice2(adapter,
+                                                'HttpOnOffDevice2-03',
+                                                "sonoff3"));
+}
+
+module.exports = loadHttpOnOffAdapter2;
